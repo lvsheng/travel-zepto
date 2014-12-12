7be36035fae74fb6258510db36d2b8c0520bd853
lvsheng <lvsheng@baidu.com>
2014-12-13 01:18:08 +0800

    add .fn namespace

--- zepto.js
+++ zepto.js
@@ -1,33 +1,32 @@
-var $ = function (_) {
+function $ (_) {
     if (typeof _ === 'function') {
         $.dom.forEach(_);
     } else {
         $.dom = [].slice.apply(document.querySelectorAll(_));
     }
 
-    return $;
-};
+    return $.fn;
+}
 
-$.html = function (html) {
-    return $(function (el) {
-        el.innerHTML = html;
-    });
-};
-
-$.append = function (html) {
-    return $(function (el) {
-        el.insertAdjacentHTML('beforeEnd', html);
-    });
-};
-
-$.prepend = function (html) {
-    return $(function (el) {
-        el.insertAdjacentHTML('afterBegin', html);
-    });
-};
-
-$.css = function (style) {
-    return $(function (el) {
-        el.style.cssText += ';' + style;
-    });
+$.fn = {
+    html: function (html) {
+        return $(function (el) {
+            el.innerHTML = html;
+        });
+    },
+    append: function (html) {
+        return $(function (el) {
+            el.insertAdjacentHTML('beforeEnd', html);
+        });
+    },
+    prepend: function (html) {
+        return $(function (el) {
+            el.insertAdjacentHTML('afterBegin', html);
+        });
+    },
+    css: function (style) {
+        return $(function (el) {
+            el.style.cssText += ';' + style;
+        });
+    }
 };

2c4f3bbed7546937e7f3f1016d9bda0e1b98f865
lvsheng <lvsheng@baidu.com>
2014-12-12 23:13:21 +0800

    add append and prepend

--- zepto.js
+++ zepto.js
@@ -11,6 +11,22 @@ $.html = function (html) {
     return $;
 };
 
+$.append = function (html) {
+    $.dom.forEach(function (el) {
+        el.insertAdjacentHTML('beforeEnd', html);
+    });
+
+    return $;
+};
+
+$.prepend = function (html) {
+    $.dom.forEach(function (el) {
+        el.insertAdjacentHTML('afterBegin', html);
+    });
+
+    return $;
+};
+
 $.css = function (style) {
     $.dom.forEach(function (el) {
         el.style.cssText += ';' + style;

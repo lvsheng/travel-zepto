9b4cd53905c53d180b5f275bed8d750cf889ca1a
lvsheng <lvsheng@baidu.com>
2014-12-13 23:34:04 +0800

    slight refactor, and add before/after

--- zepto.js
+++ zepto.js
@@ -1,63 +1,44 @@
-(function (slice) {
-    var $ = function (_) {
+var $ = (function () {
+    var slice = [].slice, k,
+        ADJ_OPS = {append: 'beforeEnd', prepend: 'afterBegin', before: 'beforeBegin', after: 'afterEnd'};
+
+    function $ (_) {
         if (typeof _ === 'function') {
             $.dom.forEach(_);
         } else {
-            $._ = _;
-            $.dom = slice.call(document.querySelectorAll(_));
+            $.dom = slice.call(document.querySelectorAll($._ = _));
         }
 
         return $.fn;
-    };
+    }
 
     $.fn = {
-        get: function (index) {
-            return index === undefined ? $.dom : $.dom[index];
-        },
+        get: function (idx) { return idx === undefined ? $.dom : $.dom[idx]; },
         html: function (html) {
-            return $(function (el) {
-                el.innerHTML = html;
-            });
-        },
-        append: function (html) {
-            return $(function (el) {
-                el.insertAdjacentHTML('beforeEnd', html);
-            });
-        },
-        prepend: function (html) {
-            return $(function (el) {
-                el.insertAdjacentHTML('afterBegin', html);
-            });
+            return $(function (el) { el.innerHTML = html; });
         },
         css: function (style) {
-            return $(function (el) {
-                el.style.cssText += ';' + style;
-            });
+            return $(function (el) { el.style.cssText += ';' + style; });
+        },
+        anim: function (transform, opacity, dur) {
+            return $.fn.css('transition: all ' + (dur || 0.5) + 's;' +
+            'transform: ' + transform + '; opacity: ' + (opacity === 0 ? 0 : opacity || 1));
         },
         live: function (event, callback) {
             var selector = $._;
             document.body.addEventListener(event, function (event) {
-                var target = event.target;
-                var nodes = slice.call(document.querySelectorAll(selector));
-                while (target && nodes.indexOf(target) < 0) {
-                    target = target.parentNode;
-                }
-
-                if (target && target !== document) {
-                    callback.call(target, event);
-                }
+                var target = event.target, nodes = slice.call(document.querySelectorAll(selector));
+                while (target && nodes.indexOf(target) < 0) { target = target.parentNode; }
+                if (target && target !== document) callback.call(target, event);
             }, false);
-
             return $.fn;
-        },
-        anim: function (transform, opacity, dur) {
-            return $.fn.css(
-                'transition: all ' + (dur || 0.5) + 's;' +
-                'transform: ' + transform + '; opacity: ' + (opacity === 0 ? 0 : opacity || 1)
-            );
         }
     };
 
+    for (k in ADJ_OPS)
+        $.fn[k] = (function (op) {
+            return function (html) { return $(function (el) { el.insertAdjacentHTML(op,html) }) };
+        })(ADJ_OPS[k]);
 
     function ajax (method, url, success) {
         var r = new XMLHttpRequest();
@@ -82,5 +63,5 @@
         });
     };
 
-    this.$ = $;
-})([].slice);
+    return $;
+})();

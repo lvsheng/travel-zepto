41d82e870ea762a131762e0032c17138398ae221
lvsheng <lvsheng@baidu.com>
2014-12-15 22:33:18 +0800

    refactor and make sure it behaves like jQuery does

--- zepto.js
+++ zepto.js
@@ -16,12 +16,11 @@ var $ = (function (d) {
         remove: function () { return this(function (el) { el.parentNode.removeChild(el) }) },
         each: function (callback) { return this(function (el) { callback(el) }) },
         html: function (html) {
-            if (html === undefined) { return this.dom[0] ? this.dom[0].innerHTML : undefined }
-            else { return this(function (el) { el.innerHTML = html; }); }
+            return (html === undefined) ? this.dom[0].innerHTML : this(function (el) { el.innerHTML = html });
         },
         attr: function (name, value) {
-            if (value === undefined) { return this.dom[0].getAttribute(name) || undefined; }
-            else { return this(function (el) { el.setAttribute(name, value); }); }
+            return (value === undefined) ? this.dom[0].getAttribute(name) || undefined :
+                this(function (el) { el.setAttribute(name, value) });
         },
         css: function (style) {
             return this(function (el) { el.style.cssText += ';' + style; });

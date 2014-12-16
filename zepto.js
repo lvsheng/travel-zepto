f4780a4a0fedeb13e2f0c0be06f58f7aba92e340
lvsheng <lvsheng@baidu.com>
2014-12-16 22:51:23 +0800

    Add .width() and .height()

--- zepto.js
+++ zepto.js
@@ -77,6 +77,8 @@ var $ = (function (d) {
         }
     };
 
+    ['width', 'height'].forEach(function(m){ $.fn[m] = function(){ return this.offset()[m] }});
+
     for (k in ADJ_OPS)
         $.fn[k] = (function (op) {
             return function (html) { return this(function (el) {
